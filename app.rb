219726385ff1c0b9a52c0e7856146f144b5483a4
Mike Chlipala <mike@chlipala.com>
2013-06-07 11:20:47 -0700

    basic auth

--- app.rb
+++ app.rb
@@ -5,16 +5,32 @@ require 'sequel'
 
 DB = Sequel.connect(ENV['DATABASE_URL'])
 
+helpers do
+  def protected!
+    return if authorized?
+    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    halt 401, "Not authorized\n"
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == [ENV['USER'], ENV['PASSWORD']]
+  end
+end
+
 get '/' do
   @thing = DB['SELECT * FROM things ORDER BY random() LIMIT 1'].first
   erb :index
 end
 
 get '/things/new' do
+  protected!
   erb :new
 end
 
 post '/things' do
+  protected!
   DB[:things].insert(:name => params['name'], :url => params['url'])
   redirect '/'
 end
+
