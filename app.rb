b26fbcafc3ca684ccfe2531ddbd36090a063903e
Mike Chlipala <mike@chlipala.com>
2013-06-06 21:42:27 -0700

    can submit things now

--- app.rb
+++ app.rb
@@ -9,3 +9,12 @@ get '/' do
   @thing = DB['SELECT * FROM things ORDER BY random() LIMIT 1'].first
   erb :index
 end
+
+get '/things/new' do
+  erb :new
+end
+
+post '/things' do
+  DB[:things].insert(:name => params['name'], :url => params['url'])
+  redirect '/'
+end
