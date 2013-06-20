beb2f45907f235e53c0bbe51f29eb7c6afe536e5
Mike Chlipala <mike@chlipala.com>
2013-06-19 21:12:13 -0700

    NOT NYAN *CAT

--- db/seeds/things.rb
+++ db/seeds/things.rb
@@ -1,1 +1,1 @@
-Thing.insert(name: "NOT NYAN", url: "http://bit.ly/13U5aPL") unless Thing.where(name: "NOT NYAN").first
+Thing.insert(name: "NOT NYAN CAT", url: "http://bit.ly/13U5aPL") unless Thing.where(name: "NOT NYAN").first
